b30d04eb19f7b2130fafbefeb0d57db55b5dabdf
eduardogm81qg <QuarkGluon123>
2018-04-10 09:29:14 +0200

    Ejemplo de uso de la libreria Axios (para realizar peticiones Http) para llamar a una Api de geolocalización de google.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const argv = require('yargs').options({
     direccion: {
         alias: 'd',
@@ -6,4 +7,16 @@ const argv = require('yargs').options({
     }
 }).argv;
 
-console.log(argv.direccion);
+let encodedUrl = encodeURI(argv.direccion);
+
+axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${ encodedUrl }&key=AIzaSyASPiaw4FAKInDRrhGA33Ei6FmS4-4YQ9s`)
+    .then( resp => {
+        // console.log( JSON.stringify(resp.data, null, 2));
+        // console.log(resp.status);
+        let location = resp.data.results[0];
+        let coors = location.geometry.location;
+        console.log('Direccion:', location.formatted_address);
+        console.log('Latitud:', coors.lat);
+        console.log('Longitud:', coors.lng);
+    } )
+    .catch( e => console.log('ERROR!!!', e) );
