869e9d1d2d1e088f63f0e8c19a94eb4d1d9cc356
eduardogm81qg <QuarkGluon123>
2018-04-10 09:47:00 +0200

    Se han separado en modulos las llamas al yargs y a la geolocalización de google

--- app.js
+++ app.js
@@ -1,22 +1,8 @@
-const axios = require('axios');
-const argv = require('yargs').options({
-    direccion: {
-        alias: 'd',
-        desc: 'Dirección de la ciudad para obtener el clima',
-        demand: true
-    }
-}).argv;
+const argv = require('./config/yargs').argv;
+const lugar = require('./lugar/lugar');
 
-let encodedUrl = encodeURI(argv.direccion);
-
-axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${ encodedUrl }&key=AIzaSyASPiaw4FAKInDRrhGA33Ei6FmS4-4YQ9s`)
+lugar.getLugarLatLng(argv.direccion)
     .then( resp => {
-        // console.log( JSON.stringify(resp.data, null, 2));
-        // console.log(resp.status);
-        let location = resp.data.results[0];
-        let coors = location.geometry.location;
-        console.log('Direccion:', location.formatted_address);
-        console.log('Latitud:', coors.lat);
-        console.log('Longitud:', coors.lng);
+        console.log(JSON.stringify(resp, undefined, 2));
     } )
-    .catch( e => console.log('ERROR!!!', e) );
+    .catch(e => console.log('ERROR!!!', e));
